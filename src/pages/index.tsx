f1b21d721d2bb1c65ffda16c227f086ec9b38069
rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-03-16 14:42:23 -0500

    Minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           <div className="flex mt-4 space-x-4">
             <SocialLink href="https://github.com/rapphy243" icon={<FaGithub className="w-7 h-7"/>} label="GitHub" />
             <SocialLink href="https://youtube.com/@243" icon={<FaYoutube className="w-7 h-7"/>} label="YouTube" colorClass="text-red-500" />
-            <SocialLink href="/about" icon={<FaRegAddressCard className="w-7 h-7"/>} label="About" />
+            <SocialLink href="./about" icon={<FaRegAddressCard className="w-7 h-7"/>} label="About" />
           </div>
         </motion.div>
         <motion.div className="fixed bottom-4 right-4"
