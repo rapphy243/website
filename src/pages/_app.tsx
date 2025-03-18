43c2f8031ab46d4ccbe709573ec2568fa209ccfa
Rapphy <36800640+rapphy243@users.noreply.github.com>
2025-03-18 16:12:47 +0000

    Add plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { sen } from "../util/fonts";
 import { type AppType } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { useEffect } from "react";
-
+import PlausibleProvider from 'next-plausible'
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
@@ -11,9 +11,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   }, []);
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableColorScheme>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PlausibleProvider domain="rap.ph" customDomain="https://plausible.rapph.dev">
+      <ThemeProvider attribute="class" defaultTheme="system" enableColorScheme>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 };
 
